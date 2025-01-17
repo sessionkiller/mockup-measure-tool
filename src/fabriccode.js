2ee363e48e35c8df501c010a48cdf7332c2a2f00
AzureAD\EstherCidVara <ecid@hiberus.com>
2025-01-17 10:43:56 +0100

    add the enable disable events to the canvas

--- src/fabriccode.js
+++ src/fabriccode.js
@@ -102,3 +102,77 @@ canvas.on('mouse:move', function (o) {
 
 // Renderizar el canvas
 canvas.renderAll();
+
+document.getElementById('clear').addEventListener('click', function () {
+    canvas.clear();
+    canvas.setBackgroundColor('#f0f0f0', canvas.renderAll.bind(canvas));
+});
+
+
+// DESHABILITAR LA EDICION DEL CANVAS
+
+const navbar = document.getElementById('navbar');
+const button = document.createElement('button');
+button.innerText = 'Click Me';
+let eventsEnabled = true;
+
+button.addEventListener('click', function () {
+    if (eventsEnabled) {
+        alert('Events disabled!');
+        canvas.off('mouse:down');
+        canvas.off('mouse:move');
+    } else {
+        alert('Events enabled!');
+        canvas.on('mouse:down', function (o) {
+            const pointer = canvas.getPointer(o.e);
+            if (!isDrawing) {
+                isDrawing = true;
+                startX = pointer.x;
+                startY = pointer.y;
+                const startPoint = new fabric.Circle({
+                    left: startX - 5,
+                    top: startY - 5,
+                    radius: 5,
+                    fill: 'blue',
+                    selectable: false,
+                    evented: false
+                });
+                canvas.add(startPoint);
+            } else {
+                isDrawing = false;
+                canvas.remove(tempLine, tempText);
+                addLine(startX, startY, pointer.x, pointer.y);
+            }
+        });
+
+        canvas.on('mouse:move', function (o) {
+            if (!isDrawing) return;
+
+            const pointer = canvas.getPointer(o.e);
+            if (tempLine) {
+                canvas.remove(tempLine, tempText);
+            }
+
+            tempLine = new fabric.Line([startX, startY, pointer.x, pointer.y], {
+                stroke: 'black',
+                selectable: false,
+                evented: false
+            });
+
+            const length = Math.sqrt(Math.pow(pointer.x - startX, 2) + Math.pow(pointer.y - startY, 2)).toFixed(2);
+            tempText = new fabric.Text(length + ' px', {
+                left: (startX + pointer.x) / 2,
+                top: (startY + pointer.y) / 2,
+                fontSize: 14,
+                fill: 'red',
+                selectable: false,
+                evented: false
+            });
+
+            canvas.add(tempLine, tempText);
+            canvas.renderAll();
+        });
+    }
+    eventsEnabled = !eventsEnabled;
+});
+navbar.appendChild(button);
